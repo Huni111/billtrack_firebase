faabf9f57e572d5587f8415d7055ad9b5ddc7c4c
Huni111 <huni19966@gmail.com>
2025-06-02 15:10:04 +0300

    date

--- src/Bills.jsx
+++ src/Bills.jsx
@@ -3,22 +3,28 @@ import data from '../data.json/'
 
 export default function Bills() {
 
-    const 
+    const today = new Date();
+    const currentYear = today.getFullYear()
+    const currentMonth = today.getMonth()+1
+    const data_facturi = new Date(data.data_facturi)
 
-    const elso = data.sort((a, b) => (a.valoare_totala - b.valoare_totala)).slice(0,10)
+    let elso10 = data.sort((a, b) => (a.valoare_totala - b.valoare_totala)).slice(0,10)
+    let havi_bejovo = 
 
     useEffect(() => {
+
 
         
 
         return(
-        console.log(elso)
+        console.log(data_facturi)
         )
     },[])
 
     return (
         <>
-        <div>bills</div>
+        <div>{data_facturi}</div>
+        <div>hej</div>
 
         </>
 
